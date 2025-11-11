67a5e4ebf97f90cc743f7eaed2b5b1d45b9d7be8
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2025-11-12 00:15:22 +0300

    add pytest fixture for test client and update requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,18 @@
 import sys
 from pathlib import Path
 
+import pytest
+from starlette.testclient import TestClient
+
+from app.main import app
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as c:
+        yield c
+
+
 ROOT = Path(__file__).resolve().parents[1]  # корень репозитория
 if str(ROOT) not in sys.path:
     sys.path.insert(0, str(ROOT))
